6f35fd375f7a0d6b43167b55833aa2b0b0a38f54
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-17 18:55:49 -0300

    test: :test_tube: add andspecification unit tests
    introduces unit tests for andspecification, covering scenarios with all, partial, and no specifications satisfied, including edge cases with no specifications and empty candidates.

---
+++ pkg/and_specification_test.go
@@ -0,0 +1,75 @@
+package specification
+
+import (
+	"testing"
+
+	"github.com/mateusmacedo/vibranium/specification/pkg/contract"
+	"github.com/stretchr/testify/assert"
+
+	mock_contract "github.com/mateusmacedo/vibranium/specification/tests/mocks/contract"
+)
+
+func TestAndSpecification_IsSatisfiedBy(t *testing.T) {
+	tests := []struct {
+		name       string
+		candidate  string
+		specs      []func(candidate string) bool
+		expected   bool
+	}{
+		{
+			name:      "AllSpecificationsSatisfied",
+			candidate: "candidate",
+			specs: []func(candidate string) bool{
+				func(candidate string) bool { return true },
+				func(candidate string) bool { return true },
+			},
+			expected: true,
+		},
+		{
+			name:      "OneSpecificationNotSatisfied",
+			candidate: "candidate",
+			specs: []func(candidate string) bool{
+				func(candidate string) bool { return true },
+				func(candidate string) bool { return false },
+			},
+			expected: false,
+		},
+		{
+			name:      "NoSpecifications",
+			candidate: "candidate",
+			specs:     []func(candidate string) bool{},
+			expected:  true,
+		},
+		{
+			name:      "EmptyCandidate",
+			candidate: "",
+			specs: []func(candidate string) bool{
+				func(candidate string) bool { return true },
+				func(candidate string) bool { return true },
+			},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var specs []contract.Specification[string]
+			for _, specFunc := range tt.specs {
+				mockSpec := new(mock_contract.MockSpecification[string])
+				mockSpec.On("IsSatisfiedBy", tt.candidate).Return(specFunc(tt.candidate))
+				specs = append(specs, mockSpec)
+			}
+
+			andSpec := NewAndSpecification[string](specs...)
+
+			// Act
+			result := andSpec.IsSatisfiedBy(tt.candidate)
+
+			// Assert
+			assert.Equal(t, tt.expected, result)
+			for _, spec := range specs {
+				spec.(*mock_contract.MockSpecification[string]).AssertExpectations(t)
+			}
+		})
+	}
+}
