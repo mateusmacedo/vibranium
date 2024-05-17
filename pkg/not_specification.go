be2a0607c6bfcce0c51c67a194391f094179b5c1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-17 20:59:26 -0300

    feat: :sparkles: add notspecification class
    introduces a class to apply logical negation on specifications

---
+++ pkg/not_specification.go
@@ -0,0 +1,15 @@
+package specification
+
+import "github.com/mateusmacedo/vibranium/specification/pkg/contract"
+
+type NotSpecification[T contract.Candidate] struct {
+	spec contract.Specification[T]
+}
+
+func NewNotSpecification[T contract.Candidate](spec contract.Specification[T]) *NotSpecification[T] {
+	return &NotSpecification[T]{spec: spec}
+}
+
+func (s *NotSpecification[T]) IsSatisfiedBy(candidate T) bool {
+	return !s.spec.IsSatisfiedBy(candidate)
+}
