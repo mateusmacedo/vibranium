9ed3e35110d46717c034acabbb73af18756e8566
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-17 23:54:16 -0300

    style: :gem: standardize code indentation
    Reformatted code to use consistent indentation across the module for improved readability and maintainability.

--- pkg/or_specification_test.go
+++ pkg/or_specification_test.go
@@ -1,3 +1,4 @@
+// file: pkg/or_specification_test.go
 package specification
 
 import (
@@ -10,87 +11,87 @@ import (
 )
 
 func TestOrSpecification_IsSatisfiedBy(t *testing.T) {
-	tests := []struct {
-		name      string
-		candidate string
-		specs     []func(candidate string) bool
-		expected  bool
-	}{
-		{
-			name:      "AllSpecificationsSatisfied",
-			candidate: "candidate",
-			specs: []func(candidate string) bool{
-				func(candidate string) bool { return true },
-				func(candidate string) bool { return true },
-			},
-			expected: true,
-		},
-		{
-			name:      "OneSpecificationSatisfied",
-			candidate: "candidate",
-			specs: []func(candidate string) bool{
-				func(candidate string) bool { return false },
-				func(candidate string) bool { return true },
-			},
-			expected: true,
-		},
-		{
-			name:      "NoSpecificationsSatisfied",
-			candidate: "candidate",
-			specs: []func(candidate string) bool{
-				func(candidate string) bool { return false },
-				func(candidate string) bool { return false },
-			},
-			expected: false,
-		},
-		{
-			name:      "NoSpecifications",
-			candidate: "candidate",
-			specs:     []func(candidate string) bool{},
-			expected:  false,
-		},
-		{
-			name:      "EmptyCandidate",
-			candidate: "",
-			specs: []func(candidate string) bool{
-				func(candidate string) bool { return true },
-				func(candidate string) bool { return true },
-			},
-			expected: true,
-		},
-	}
+    tests := []struct {
+        name      string
+        candidate string
+        specs     []func(candidate string) bool
+        expected  bool
+    }{
+        {
+            name:      "AllSpecificationsSatisfied",
+            candidate: "candidate",
+            specs: []func(candidate string) bool{
+                func(candidate string) bool { return true },
+                func(candidate string) bool { return true },
+            },
+            expected: true,
+        },
+        {
+            name:      "OneSpecificationSatisfied",
+            candidate: "candidate",
+            specs: []func(candidate string) bool{
+                func(candidate string) bool { return false },
+                func(candidate string) bool { return true },
+            },
+            expected: true,
+        },
+        {
+            name:      "NoSpecificationsSatisfied",
+            candidate: "candidate",
+            specs: []func(candidate string) bool{
+                func(candidate string) bool { return false },
+                func(candidate string) bool { return false },
+            },
+            expected: false,
+        },
+        {
+            name:      "NoSpecifications",
+            candidate: "candidate",
+            specs:     []func(candidate string) bool{},
+            expected:  false,
+        },
+        {
+            name:      "EmptyCandidate",
+            candidate: "",
+            specs: []func(candidate string) bool{
+                func(candidate string) bool { return true },
+                func(candidate string) bool { return true },
+            },
+            expected: true,
+        },
+    }
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var specs []contract.Specification[string]
-			var mocks []*mock_contract.MockSpecification[string]
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            var specs []contract.Specification[string]
+            var mocks []*mock_contract.MockSpecification[string]
 
-			for _, specFunc := range tt.specs {
-				mockSpec := new(mock_contract.MockSpecification[string])
-				mockSpec.On("IsSatisfiedBy", tt.candidate).Return(specFunc(tt.candidate)).Once()
-				specs = append(specs, mockSpec)
-				mocks = append(mocks, mockSpec)
-			}
+            for _, specFunc := range tt.specs {
+                mockSpec := new(mock_contract.MockSpecification[string])
+                mockSpec.On("IsSatisfiedBy", tt.candidate).Return(specFunc(tt.candidate)).Once()
+                specs = append(specs, mockSpec)
+                mocks = append(mocks, mockSpec)
+            }
 
-			orSpec := NewOrSpecification[string](specs...)
+            orSpec := NewOrSpecification[string](specs...)
 
-			// Act
-			result := orSpec.IsSatisfiedBy(tt.candidate)
+            // Act
+            result := orSpec.IsSatisfiedBy(tt.candidate)
 
-			// Assert
-			assert.Equal(t, tt.expected, result)
+            // Assert
+            assert.Equal(t, tt.expected, result)
 
-			// Verificar expectativas
-			for i, mock := range mocks {
-				if tt.expected && i > 0 && tt.specs[i-1](tt.candidate) == true {
-					// Se o resultado esperado for verdadeiro e a especificação anterior retornou verdadeiro,
-					// verificamos que esta especificação não foi chamada
-					mock.AssertNotCalled(t, "IsSatisfiedBy", tt.candidate)
-				} else {
-					// Caso contrário, verificamos que a especificação foi chamada
-					mock.AssertExpectations(t)
-				}
-			}
-		})
-	}
+            // Verificar expectativas
+            for i, mock := range mocks {
+                if tt.expected && i > 0 && tt.specs[i-1](tt.candidate) {
+                    // Se o resultado esperado for verdadeiro e a especificação anterior retornou verdadeiro,
+                    // verificamos que esta especificação não foi chamada
+                    mock.AssertNotCalled(t, "IsSatisfiedBy", tt.candidate)
+                } else {
+                    // Caso contrário, verificamos que a especificação foi chamada
+                    mock.AssertExpectations(t)
+                }
+            }
+        })
+    }
 }
