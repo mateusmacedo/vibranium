ee0ce477b0f2f337e9be3eadb621e00f4e7df7c1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-17 23:54:28 -0300

    test: :test_tube: update specification tests
    refined test structure in specifications_test.go for clarity and consistency in mock specifications handling.

--- pkg/specifications_test.go
+++ pkg/specifications_test.go
@@ -1,3 +1,4 @@
+// file: pkg/specifications_test.go
 package specification
 
 import (
@@ -11,173 +12,172 @@ import (
 )
 
 type specificationIntegrationTest struct {
-	name      string
-	candidate string
-	expected  bool
-	finalSpec contract.Specification[string]
+    name      string
+    candidate string
+    expected  bool
+    finalSpec contract.Specification[string]
 }
 
 func TestSpecificationIntegration(t *testing.T) {
-	tests := []specificationIntegrationTest{
-		{
-			name:      "AllSpecificationsSatisfied",
-			candidate: "candidate",
-			expected:  true,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "OneAndSpecificationNotSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(false),
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "OneOrSpecificationSatisfied",
-			candidate: "candidate",
-			expected:  true,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(false),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "AllOrSpecificationsNotSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(false),
-					createMockSpec(false),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "NotSpecificationSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(true)),
-			),
-		},
-		{
-			name:      "NoSpecifications",
-			candidate: "candidate",
-			expected:  true,
-			finalSpec: NewAndSpecification[string](
-				NewOrSpecification[string](
-					NewNotSpecification[string](createMockSpec(false)),
-				),
-			),
-		},
-		{
-			name:      "EmptyCandidate",
-			candidate: "",
-			expected:  true,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		// Novos cenários
-		{
-			name:      "AllAndSpecificationsNotSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(false),
-				createMockSpec(false),
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(true),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "AllAndSpecificationsSatisfiedAndNotSpecSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				createMockSpec(true),
-				createMockSpec(true),
-				NewOrSpecification[string](
-					createMockSpec(false),
-					createMockSpec(false),
-				),
-				NewNotSpecification[string](createMockSpec(true)),
-			),
-		},
-		{
-			name:      "NoAndSpecificationsAndOrSpecSatisfied",
-			candidate: "candidate",
-			expected:  true,
-			finalSpec: NewAndSpecification[string](
-				NewOrSpecification[string](
-					createMockSpec(true),
-					createMockSpec(false),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-		{
-			name:      "NoAndSpecificationsAndOrSpecNotSatisfied",
-			candidate: "candidate",
-			expected:  false,
-			finalSpec: NewAndSpecification[string](
-				NewOrSpecification[string](
-					createMockSpec(false),
-					createMockSpec(false),
-				),
-				NewNotSpecification[string](createMockSpec(false)),
-			),
-		},
-	}
+    tests := []specificationIntegrationTest{
+        {
+            name:      "AllSpecificationsSatisfied",
+            candidate: "candidate",
+            expected:  true,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "OneAndSpecificationNotSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(false),
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "OneOrSpecificationSatisfied",
+            candidate: "candidate",
+            expected:  true,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(false),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "AllOrSpecificationsNotSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(false),
+                    createMockSpec(false),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "NotSpecificationSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(true)),
+            ),
+        },
+        {
+            name:      "NoSpecifications",
+            candidate: "candidate",
+            expected:  true,
+            finalSpec: NewAndSpecification[string](
+                NewOrSpecification[string](
+                    NewNotSpecification[string](createMockSpec(false)),
+                ),
+            ),
+        },
+        {
+            name:      "EmptyCandidate",
+            candidate: "",
+            expected:  true,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "AllAndSpecificationsNotSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(false),
+                createMockSpec(false),
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(true),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "AllAndSpecificationsSatisfiedAndNotSpecSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                createMockSpec(true),
+                createMockSpec(true),
+                NewOrSpecification[string](
+                    createMockSpec(false),
+                    createMockSpec(false),
+                ),
+                NewNotSpecification[string](createMockSpec(true)),
+            ),
+        },
+        {
+            name:      "NoAndSpecificationsAndOrSpecSatisfied",
+            candidate: "candidate",
+            expected:  true,
+            finalSpec: NewAndSpecification[string](
+                NewOrSpecification[string](
+                    createMockSpec(true),
+                    createMockSpec(false),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+        {
+            name:      "NoAndSpecificationsAndOrSpecNotSatisfied",
+            candidate: "candidate",
+            expected:  false,
+            finalSpec: NewAndSpecification[string](
+                NewOrSpecification[string](
+                    createMockSpec(false),
+                    createMockSpec(false),
+                ),
+                NewNotSpecification[string](createMockSpec(false)),
+            ),
+        },
+    }
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result := tt.finalSpec.IsSatisfiedBy(tt.candidate)
-			assert.Equal(t, tt.expected, result)
-		})
-	}
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            result := tt.finalSpec.IsSatisfiedBy(tt.candidate)
+            assert.Equal(t, tt.expected, result)
+        })
+    }
 }
 
 func createMockSpec(result bool) contract.Specification[string] {
-	mockSpec := new(mock_contract.MockSpecification[string])
-	mockSpec.On("IsSatisfiedBy", mock.Anything).Return(result)
-	return mockSpec
+    mockSpec := new(mock_contract.MockSpecification[string])
+    mockSpec.On("IsSatisfiedBy", mock.Anything).Return(result)
+    return mockSpec
 }
