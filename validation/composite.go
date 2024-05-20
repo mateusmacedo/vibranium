d31f118c2ee082b6a24fec8f70011ffbfb5a3c49
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-20 03:45:43 -0300

    refactor: :hammer: rename validator struct type
    renamed ValidatorWithContext to WithField to clarify usage within composite validator structures.

--- validation/composite.go
+++ validation/composite.go
@@ -6,20 +6,20 @@ import (
 )
 
 type Composite[T any] struct {
-	validators []ValidatorWithContext[T]
+	validators []WithField[T]
 }
 
-type ValidatorWithContext[T any] struct {
+type WithField[T any] struct {
 	Field     string
 	Validator contract.Validator[T]
 }
 
 func NewComposite[T any]() *Composite[T] {
-	return &Composite[T]{validators: []ValidatorWithContext[T]{}}
+	return &Composite[T]{validators: []WithField[T]{}}
 }
 
 func (c *Composite[T]) AddValidator(field string, validator contract.Validator[T]) {
-	c.validators = append(c.validators, ValidatorWithContext[T]{Field: field, Validator: validator})
+	c.validators = append(c.validators, WithField[T]{Field: field, Validator: validator})
 }
 
 func (c *Composite[T]) Validate(value T) error {
