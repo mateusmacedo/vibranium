806f7131d8dd7dc368c01e0e4728b8af4caecf9e
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-20 00:49:45 -0300

    refactor: :hammer: rename ValidatorFunc to ValidationFunc
    Renamed type to improve semantic clarity in validation logic.

--- validation/contract/validator.go
+++ validation/contract/validator.go
@@ -6,8 +6,8 @@ type Validator[T Value] interface {
     Validate(value T) error
 }
 
-type ValidatorFunc[T Value] func(value T) error
+type ValidationFunc[T Value] func(value T) error
 
-func (f ValidatorFunc[T]) Validate(value T) error {
+func (f ValidationFunc[T]) Validate(value T) error {
     return f(value)
 }
