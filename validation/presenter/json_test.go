744cb89424e1dd8bd397559203690a517b2c3d2d
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-20 04:21:05 -0300

    test: :test_tube: add json presenter tests
    introduces unit tests for json error presentation handling

---
+++ validation/presenter/json_test.go
@@ -0,0 +1,87 @@
+package presenter
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mateusmacedo/vibranium/validation/errors"
+)
+
+func TestJSONPresenter(t *testing.T) {
+	tests := []struct {
+		name          string
+		initialErrors *errors.Errors
+		expectedOutput string
+	}{
+		{
+			name: "No errors",
+			initialErrors: &errors.Errors{},
+			expectedOutput: "{}",
+		},
+		{
+			name: "Single error",
+			initialErrors: &errors.Errors{
+				List: []errors.Error{
+					{Field: "Name", Err: "value cannot be empty"},
+				},
+			},
+			expectedOutput: `{
+  "Name": [
+    "value cannot be empty"
+  ]
+}`,
+		},
+		{
+			name: "Multiple errors",
+			initialErrors: &errors.Errors{
+				List: []errors.Error{
+					{Field: "Name", Err: "value cannot be empty"},
+					{Field: "Age", Err: "value must be positive"},
+				},
+			},
+			expectedOutput: `{
+  "Name": [
+    "value cannot be empty"
+  ],
+  "Age": [
+    "value must be positive"
+  ]
+}`,
+		},
+		{
+			name: "Nested errors",
+			initialErrors: &errors.Errors{
+				List: []errors.Error{
+					{Field: "Address.Street", Err: "value cannot be empty"},
+					{Field: "Address.Zip", Err: "value must be positive"},
+				},
+			},
+			expectedOutput: `{
+  "Address": {
+    "Street": [
+      "value cannot be empty"
+    ],
+    "Zip": [
+      "value must be positive"
+    ]
+  }
+}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			presenter := &JSONPresenter{}
+
+			output := presenter.Present(tt.initialErrors)
+
+			var expected, actual map[string]interface{}
+			json.Unmarshal([]byte(tt.expectedOutput), &expected)
+			json.Unmarshal([]byte(output), &actual)
+
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
