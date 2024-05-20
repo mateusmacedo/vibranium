1cde9d440a0259978da867a5dac2e6ba24a67496
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-20 01:09:26 -0300

    refactor: :hammer: enhance error struct and handling
    restructured error handling to include contextual information for each error, improving debugging and logging capabilities.

--- validation/errors.go
+++ validation/errors.go
@@ -1,25 +1,43 @@
 package validation
 
 import (
+	"fmt"
 	"strings"
 )
 
+type Error struct {
+    Context string
+    Err     error
+}
+
+func (ve *Error) Error() string {
+    return fmt.Sprintf("%s: %s", ve.Context, ve.Err.Error())
+}
+
 type Errors struct {
-    errors []error
+    List []Error
 }
 
 func (e *Errors) Error() string {
     var errorMessages []string
-    for _, err := range e.errors {
+    for _, err := range e.List {
         errorMessages = append(errorMessages, err.Error())
     }
     return strings.Join(errorMessages, ";\n")
 }
 
-func (e *Errors) Add(err error) {
-    e.errors = append(e.errors, err)
+func (e *Errors) Add(context string, err error) {
+    e.List = append(e.List, Error{Context: context, Err: err})
 }
 
 func (e *Errors) IsEmpty() bool {
-    return len(e.errors) == 0
+    return len(e.List) == 0
+}
+
+func IndentErrorMessages(errorMessages string, indent string) string {
+    lines := strings.Split(errorMessages, ";\n")
+    for i, line := range lines {
+        lines[i] = indent + line
+    }
+    return strings.Join(lines, ";\n")
 }
