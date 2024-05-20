4cfa16c3363810a83ba4b6d9122c99f015117a38
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-05-20 01:09:54 -0300

    refactor: :hammer: improve error details in collection validation
    enhanced error reporting by including item index and indenting error messages for better readability

--- validation/collection.go
+++ validation/collection.go
@@ -1,6 +1,10 @@
 package validation
 
-import "github.com/mateusmacedo/vibranium/validation/contract"
+import (
+	"fmt"
+
+	"github.com/mateusmacedo/vibranium/validation/contract"
+)
 
 type Collection[T any] struct {
     itemValidator contract.Validator[T]
@@ -10,11 +14,13 @@ func NewCollection[T any](itemValidator contract.Validator[T]) *Collection[T] {
     return &Collection[T]{itemValidator: itemValidator}
 }
 
-func (c *Collection[T]) Validate(items []T) error {
+func (cv *Collection[T]) Validate(items []T) error {
     errors := &Errors{}
-    for _, item := range items {
-        if err := c.itemValidator.Validate(item); err != nil {
-            errors.Add(err)
+    for i, item := range items {
+        if err := cv.itemValidator.Validate(item); err != nil {
+            context := fmt.Sprintf("Item %d", i)
+            indentedError := IndentErrorMessages(err.Error(), "  ")
+            errors.Add(context, fmt.Errorf("%s", indentedError))
         }
     }
     if errors.IsEmpty() {
